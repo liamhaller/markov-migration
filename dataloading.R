0fb384a5d216dbc0b31fcf3ced9820f44d88926e
“Liam <liamhllr2@gmail.comgit config --listgit config --listgit config --global user.name Liam Hallergit config --global user.name Liam Hallergit config --global user.email liamhllr2@gmail.com>
2024-02-28 21:36:06 +0100

    add new data paths

--- dataloading.R
+++ dataloading.R
@@ -8,16 +8,16 @@ library(readstata13) #use to open .dta files (read.dta13)
 #load ppathl dataset 
 
 #This is individual-level data like birth year, country of origin...
-ppath <- read.dta13(file="ppath.dta", convert.factors=F)
+ppath <- read.dta13(file="/Users/haller/Desktop/DeZIM/Projects/Markov Migration/ppath.dta", convert.factors=F)
 
 
 
 # Spell Data --------------------------------------------------------------
 
 #load refugee spell data
-refugspell <- read.dta13(file = "refugspell.dta", convert.dates = TRUE, convert.factors = FALSE)
+refugspell <- read.dta13(file = "/Users/haller/Desktop/DeZIM/Projects/Markov Migration/refugspell.dta", convert.dates = TRUE, convert.factors = FALSE)
 #load migrant spell data
-migspell <- read.dta13(file = "migspell.dta", convert.dates = TRUE, convert.factors = FALSE)
+migspell <- read.dta13(file = "/Users/haller/Desktop/DeZIM/Projects/Markov Migration/migspell.dta", convert.dates = TRUE, convert.factors = FALSE)
 
 
 
